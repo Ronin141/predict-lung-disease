64c222e74d14e91730b865b2beeec0e87a24fe56
ronin <L.Tatsn@nipponexpress-necl.co.th>
2025-07-29 09:06:51 +0700

    Edit link of model

--- load_model.py
+++ load_model.py
@@ -2,10 +2,12 @@
 import os
 import gdown
 
-MODEL_ID = "1A2B3C4D5EfGHijkL678MNopQR"  # แทนด้วย Google Drive file ID จริง
-MODEL_PATH = "model.h5"
+MODEL_ID = "1ZLVYUb0PHTSAk6HlcGOjFa691iYLh4f5"  # แทนด้วย Google Drive file ID จริง
+MODEL_PATH = "lung_sound_binary_model.h5"
 
 def download_model():
     if not os.path.exists(MODEL_PATH):
         print("📥 Downloading model from Google Drive...")
         gdown.download(id=MODEL_ID, output=MODEL_PATH, quiet=False)
+
+# download_model()
